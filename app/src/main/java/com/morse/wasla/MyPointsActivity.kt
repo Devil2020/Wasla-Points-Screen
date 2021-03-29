60f877456e1aea4fe3ab2f5deb88ec4435b13d16
Devil2020 <Devil2020@yahoo.com>
2021-03-29 21:31:10 +0200

    add standard design and create a value animation for totalcount of Points

---
+++ app/src/main/java/com/morse/wasla/MyPointsActivity.kt
@@ -0,0 +1,34 @@
+package com.morse.wasla
+
+import android.animation.ValueAnimator
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.morse.wasla.databinding.ActivityMyPointsBinding
+
+class MyPointsActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMyPointsBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMyPointsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setupTabs()
+        valueAnimate(2510)
+    }
+
+    private fun setupTabs() {
+        binding.myPointsTl.addTab(binding.myPointsTl.newTab().setText("Spend Points"))
+        binding.myPointsTl.addTab(binding.myPointsTl.newTab().setText("My Activities"))
+    }
+
+    private fun valueAnimate(toValue: Int) {
+        val valueAnimation = ValueAnimator.ofInt(0, toValue)
+        valueAnimation.addUpdateListener {
+            binding.totalPointsTv.setText(it.getAnimatedValue().toString())
+        }
+        valueAnimation.setDuration(4000)
+        valueAnimation.start()
+    }
+
+}
